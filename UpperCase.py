2915871985d7f6b186d3bdb4148864135d53f797
wewakeprasad <wewakeprasad@gmail.com>
2019-08-21 19:44:56 +0530

    Create UpperCase.py

---
+++ UpperCase.py
@@ -0,0 +1,5 @@
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+
+for char in s:
+	if char.isUpper():
+		print(char,end=' ')
